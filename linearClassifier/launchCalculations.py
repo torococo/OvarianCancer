9a63e546ae6e5a818b058a770bb271c91c6c2e85
ms234 <maximilian.strobl@gmail.com>
2017-05-16 12:24:52 -0400

    Code to run a naive linear classifier on the cluster. Each partitioning of the training data will be run on a different node and the files collected in according sub-directories.

--- linearClassifier/launchCalculations.py
+++ linearClassifier/launchCalculations.py
@@ -19,7 +19,7 @@ def ClusterRun(nodes,ppn,mem,wallTime,jobName,runCommands,filePath):
 def naiveLinearRunBatch25(iSet):
   # Define the run parameters
   batchSize = 25
-  nEpochs = 1
+  nEpochs = 25
   scriptName = "naiveLinearClassifierJob_"+str(i)+"_BatchSize_"+str(batchSize)
   jobName =  "nLC"+str(i)+"_BatchSize_"+str(batchSize)
   print("Launching Job: "+jobName)
@@ -38,8 +38,33 @@ def naiveLinearRunBatch25(iSet):
   ClusterRun(1,1,5,"1:00:00",jobName,runCode,"/home/80014744/fluidigmProject/linearClassifier/naiveLinearClassifier/naiveClassifier_BatchSize_"+str(batchSize)+"/jobFiles/"+scriptName+".qsub")
 
 # ========================================================
+
+def naiveLinearRunBatch10(iSet):
+  # Define the run parameters
+  batchSize = 10
+  nEpochs = 25
+  scriptName = "naiveLinearClassifierJob_"+str(i)+"_BatchSize_"+str(batchSize)
+  jobName =  "nLC"+str(i)+"_BatchSize_"+str(batchSize)
+  print("Launching Job: "+jobName)
+
+  # Set up the environment
+  outDir = "naiveLinearClassifier/naiveClassifier_BatchSize_"+str(batchSize)
+  if not os.path.exists(outDir):
+    os.makedirs(outDir)
+    os.makedirs(outDir+"/models")
+    os.makedirs(outDir+"/trainingLogFiles")
+    os.makedirs(outDir+"/jobFiles")
+
+  # Prepare the pbs run file
+  runCode="cd /home/80014744/fluidigmProject/linearClassifier/\n"
+  runCode+="LD_LIBRARY_PATH=\"$HOME/my_libc_env/lib/x86_64-linux-gnu/:$HOME/my_libc_env/usr/lib64/\" $HOME/my_libc_env/lib/x86_64-linux-gnu/ld-2.17.so `which python` naiveLinearClassifier.py "+str(iSet)+" "+str(batchSize)+" "+str(nEpochs)+" "+outDir
+  ClusterRun(1,1,5,"1:00:00",jobName,runCode,"/home/80014744/fluidigmProject/linearClassifier/naiveLinearClassifier/naiveClassifier_BatchSize_"+str(batchSize)+"/jobFiles/"+scriptName+".qsub")
+
+# ========================================================
+
 # Run it
 
-nTrainingPermutations = 1
+nTrainingPermutations = 10
 for i in range(1,nTrainingPermutations+1):
   naiveLinearRunBatch25(i)
+  naiveLinearRunBatch10(i)
--- linearClassifier/naiveLinearClassifier.py
+++ linearClassifier/naiveLinearClassifier.py
@@ -8,7 +8,7 @@ import cnnUtils
 import Utils
 import sys
 import os
-import subprocess
+import shutil
 
 iSet = sys.argv[1]
 BATCH_SIZE = int(sys.argv[2])
@@ -40,17 +40,24 @@ data = np.load(dataPath, encoding = 'latin1')
 trainingSet = data[0]
 validSet = data[1]
 
+# Prepare folder to save model in
+modelDir = OUT_DIR+"/models/"+modelName+str(iSet)
+if not os.path.exists(modelDir):
+    os.makedirs(modelDir)
+
 # Train and test
-testError,testAccuracy = cnnUtils.TrainAndTestNetwork(myNet,DATA_DIR,trainingSet,validSet,testingSet,CORE_TO_OUTCOME_MAP,N_EPOCHS,BATCH_SIZE,outDir=OUT_DIR,modelName=modelName+str(iSet))
+testError,testAccuracy = cnnUtils.TrainAndTestNetwork(myNet,DATA_DIR,trainingSet,validSet,testingSet,CORE_TO_OUTCOME_MAP,N_EPOCHS,BATCH_SIZE,outDirLogs=OUT_DIR+"/trainingLogFiles",outDirModel=modelDir,modelName=modelName+str(iSet))
 
 # Record the results
-performanceArr = np.array([iSet,testError,testAccuracy],dtype=np.float32)
+performanceArr = np.array([iSet,testError,testAccuracy],dtype=np.float32).reshape((1,3))
 
 # Save the results
 np.savetxt(OUT_DIR+"performance_" + modelName + "_Set_" + str(iSet) + ".csv", performanceArr, fmt='%10.16f', delimiter=',', newline='\n')
 
 # Clean up
+OUT_DIR = os.path.expanduser('~') + "/fluidigmProject/linearClassifier/" + OUT_DIR
 # shutil.move(OUT_DIR + "/trainingError_" + modelName+str(iSet) + ".csv", OUT_DIR + "/trainingLogFiles/trainingError_" + modelName+str(iSet) + ".csv")
-subprocess.call("mv" + OUT_DIR + "/*trainingError* " + OUT_DIR + "/trainingLogFiles/")
-subprocess.call("mv" + OUT_DIR + "/*.qsub " + OUT_DIR + "/jobFiles/")
-subprocess.call("mv" + OUT_DIR + "/*.meta " + OUT_DIR + "*.index " + OUT_DIR + "*.data* " + OUT_DIR + "checkpoint " + OUT_DIR + "/models/")
+# subprocess.call("mv " + OUT_DIR + "/*.qsub " + OUT_DIR + "/jobFiles/")
+# subprocess.call("mv " + OUT_DIR + "/*"+str(iSet)+".meta " + OUT_DIR + "*"+str(iSet)+".index " + OUT_DIR + "*"+str(iSet)+".data* " + OUT_DIR + "checkpoint " + OUT_DIR + "/models/")
+
+# subprocess.call("mv " + OUT_DIR + "/*trainingError* " + OUT_DIR + "/trainingLogFiles/")
